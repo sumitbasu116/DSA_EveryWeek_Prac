7268fb15e725e055b3c0064cc7d1cbb92a0299de
sumitbasu116@gmail.com <sumitbasu116@gmail.com>
2025-03-12 17:17:48 +0530

    first commit

---
+++ src/co/sumit/design/pattern/SingletonEager.java
@@ -0,0 +1,20 @@
+package co.sumit.design.pattern;
+
+public class SingletonEager {
+
+	private static final SingletonEager INSTANCE = new SingletonEager(); // Eagerly initialized
+
+	private SingletonEager() {
+	}
+
+	public static SingletonEager getObject() {
+		return INSTANCE;
+	}
+}
+/*
+ * Suppose, interviewer asked that without writing Thread safety code, how we
+ * can achieve singleton?
+ * Then an eager initialization method is also valid.
+ * The instance is created at class loading time, making it thread-safe. But it
+ * always initializes the instance, even if it's never used.
+ */
---
+++ src/co/sumit/design/pattern/SingletonLazy.java
@@ -0,0 +1,19 @@
+package co.sumit.design.pattern;
+
+public class SingletonLazy {
+	// Volatile ensures visibility of changes across threads
+	public static volatile SingletonLazy object;
+	private SingletonLazy() {
+        // Private constructor to prevent instantiation
+    }
+	public static SingletonLazy getInstance() {
+		if(object==null) {
+			synchronized (SingletonLazy.class) {
+				if(object==null) {
+					object = new SingletonLazy();
+				}
+			}
+		}
+		return object;
+	}
+}
