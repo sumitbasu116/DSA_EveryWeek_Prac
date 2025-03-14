f078eb5b5ebd1db2e35acec2c2db234a8da81411
sumitbasu116@gmail.com <sumitbasu116@gmail.com>
2025-03-14 09:05:39 +0530

    WaveArray my solution

---
+++ src/co/sumit/DSA/mostimportant/GFG/WaveArray.java
@@ -0,0 +1,38 @@
+package co.sumit.DSA.mostimportant.GFG;
+
+public class WaveArray {
+
+	
+	
+	public static void convertToWave(int[] arr) {
+        int len=0;
+		if(arr.length%2==0) {
+        	len = arr.length/2;
+        }else {
+        	len = arr.length/2;
+        }
+		for(int i=0,j=0;i<len;i++,j=j+2) {
+			if(j<arr.length) {
+				swap(arr,j,j+1);
+			}
+			
+		}
+		
+    }
+	
+	public static void swap(int[] arr,int i,int j) {
+		int temp = arr[i];
+		arr[i]=arr[j];
+		arr[j]=temp;
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {1, 2, 3, 4,5,6};
+		convertToWave(arr);
+		
+		for(int e:arr) {
+			System.out.print(e+" ");
+		}
+	}
+
+}
