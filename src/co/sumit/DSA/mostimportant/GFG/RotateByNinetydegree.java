14ff7a5bd280fc1473564e1974f68a576f297aa1
sumitbasu116@gmail.com <sumitbasu116@gmail.com>
2025-03-15 21:40:06 +0530

    RotateByNinetydegree

---
+++ src/co/sumit/DSA/mostimportant/GFG/RotateByNinetydegree.java
@@ -0,0 +1,50 @@
+package co.sumit.DSA.mostimportant.GFG;
+
+//https://www.geeksforgeeks.org/most-asked-dsa-interview-problems-for-beginners/
+public class RotateByNinetydegree {
+//https://www.geeksforgeeks.org/problems/rotate-by-90-degree-1587115621/1
+	public static void main(String[] args) {
+		int n = 3;
+
+		int[][] arr = {
+				{ 1,2,3 }, 
+				{ 4,5,6 }, 
+				{ 7,8,9 }
+				};
+		rotateby90(n, arr);
+		printMatrix(arr);
+	}
+
+	private static void transpose(int n, int[][] arr) {
+		
+		for(int i=0;i<n;i++) {
+			for(int j=i+1;j<n;j++) {
+				int temp = arr[i][j];
+				arr[i][j]=arr[j][i];
+				arr[j][i]=temp;
+			}
+		}
+	
+	}
+
+	public static void printMatrix(int[][] matrix) {
+		for (int[] row : matrix) {
+			for (int num : row) {
+				System.out.print(num + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	static void rotateby90(int n , int arr[][]) {
+		transpose(n, arr);
+		
+		for(int i=0;i<n/2;i++) {
+			for(int j=0;j<n;j++) {
+				int temp=arr[i][j];
+				arr[i][j]=arr[n-i-1][j];
+				arr[n-i-1][j]=temp;
+			}
+		}
+	}
+}
